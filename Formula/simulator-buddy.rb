59957a0240d82c33194817e3f2404f2f9cf1c2ca
ipavlidakis <472467+ipavlidakis@users.noreply.github.com>
2026-04-28 17:59:53 +0000

    Update simulator-buddy to v0.4.0

--- Formula/simulator-buddy.rb
+++ Formula/simulator-buddy.rb
@@ -1,15 +1,15 @@
 class SimulatorBuddy < Formula
   desc "Interactive selector for iOS simulators and devices"
   homepage "https://github.com/ipavlidakis/simulator-buddy"
-  version "0.3.0"
+  version "0.4.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ipavlidakis/simulator-buddy/releases/download/v#{version}/simulator-buddy-aarch64-apple-darwin.tar.gz"
-      sha256 "78441eaeaa495ffc7e69c6e74fac9066739ab3f060ea195b508e4243272ede5b"
+      sha256 "6391290af2779689a4ef35085a273696df49971682c032c78eef3c55d9540035"
     else
       url "https://github.com/ipavlidakis/simulator-buddy/releases/download/v#{version}/simulator-buddy-x86_64-apple-darwin.tar.gz"
-      sha256 "02a479f6b88b2fa7b388dd0c846438450e088cf0318e63f7edc29a3307d5c126"
+      sha256 "9b0973a0163e91a68aabb866d5185f2d267ebe6725e9134911f8497c956ff709"
     end
   end
 
